7e5e45fd36c1152c549fe5128735780aa31b4ede
Alexey Ustinov <TotKtoSmog513@gmail.com>
2025-03-13 15:23:25 +0300

    Подключение приложения к БД

---
+++ SimpleAuthorization/Controllers/UserController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SimpleAuthorization.Models;
+
+namespace SimpleAuthorization.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public UserController(ApplicationDbContext context) => _context = context;
+        public IActionResult Index()
+        {
+            List<User> Users = _context.Users.ToList();
+            return View(Users);
+        }
+    }
+}
--- SimpleAuthorization/Models/ApplicationDbContext.cs
+++ SimpleAuthorization/Models/ApplicationDbContext.cs
@@ -8,6 +8,24 @@ namespace SimpleAuthorization.Models
             : base(options)
         {
         }
+        ApplicationDbContext()
+        {
+            Database.EnsureCreated();
+        }
         public DbSet<User> Users { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.ToTable("users");
+                entity.Property("Id").HasColumnName("id");
+                entity.Property("Fname").HasColumnName("fname");
+                entity.Property("Lname").HasColumnName("lname");
+                entity.Property("City").HasColumnName("city");
+                entity.Property("Phone").HasColumnName("phone");
+                entity.Property("Email").HasColumnName("email");
+                entity.Property("PasswordHash").HasColumnName("password_hash");
+            });
+        }
     }
 }
