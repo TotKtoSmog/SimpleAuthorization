0624bd2e022afc6dbea0895725a212f702d45e73
Alexey Ustinov <TotKtoSmog513@gmail.com>
2025-03-13 15:31:29 +0300

    Добавление метода хеширования пароля и метода проверки пароля

--- SimpleAuthorization/Controllers/UserController.cs
+++ SimpleAuthorization/Controllers/UserController.cs
@@ -12,5 +12,11 @@ namespace SimpleAuthorization.Controllers
             List<User> Users = _context.Users.ToList();
             return View(Users);
         }
+
+        public string HashPassword(string password) 
+            => BCrypt.Net.BCrypt.HashPassword(password);
+
+        public bool VerifyPassword(string password, string hashedPassword) 
+            => BCrypt.Net.BCrypt.Verify(password, hashedPassword);
     }
 }
