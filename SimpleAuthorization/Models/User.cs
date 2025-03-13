2207aee7f16c443b34db6f01cd32ebf565bf3dcd
Alexey Ustinov <TotKtoSmog513@gmail.com>
2025-03-13 14:55:08 +0300

    Добавление EF и создание модели данных пользователя

---
+++ SimpleAuthorization/Models/User.cs
@@ -0,0 +1,13 @@
+﻿namespace SimpleAuthorization.Models
+{
+    public class User
+    {
+        public int Id { get; set; } 
+        public string Fname { get; set; } 
+        public string Lname { get; set; } 
+        public string City { get; set; } 
+        public string Phone { get; set; } 
+        public string Email { get; set; }
+        public string PasswordHash { get; set; } 
+    }
+}
--- SimpleAuthorization/Program.cs
+++ SimpleAuthorization/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using SimpleAuthorization.Models;
+
 namespace SimpleAuthorization
 {
     public class Program
@@ -8,6 +11,10 @@ namespace SimpleAuthorization
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseNpgsql(
+                builder.Configuration.GetConnectionString("DefaultConnection")));
 
             var app = builder.Build();
 
